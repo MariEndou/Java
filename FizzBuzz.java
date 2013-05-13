d379e21ca9b0159dfab21fbf129eeb7c286cf67c
MariEndou <re_at.free_you.17@ezweb.ne.jp>
2013-05-13 17:42:55 +0900

    Added FizzBuzz
    FizzBuzzゲーム（１００まで）

---
+++ FizzBuzz.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class FizzBuzz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan  = new Scanner(System.in);
+		int count = 1;
+		int a, b;
+		System.out.println("a,ｂ,を入力してください");
+		 a = scan.nextInt();
+		 b = scan.nextInt();
+		 int c = a * b;
+		while(count < 101){
+			if(count % a == 0 && count % b != 0){
+				System.out.println("Fizz");
+			}else if(count % b == 0 && count % a != 0){
+				System.out.println("Buzz");
+			}else if(count % c == 0){
+				System.out.println("FizzBuzz");
+			}else{
+				System.out.println(count);
+			}
+			count++;
+		}
+
+	}
+
+}
