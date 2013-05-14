eb21a958b88e521a2d739a2f2bcff5d323352726
MariEndou <re_at.free_you.17@ezweb.ne.jp>
2013-05-14 16:06:43 +0900

    Added FizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class FizzBuzz {
 
 	/**
@@ -6,26 +7,31 @@ public class FizzBuzz {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan  = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		int count = 1;
 		int a, b;
 		System.out.println("a,ｂ,を入力してください");
-		 a = scan.nextInt();
-		 b = scan.nextInt();
-		 int c = a * b;
-		while(count < 101){
-			if(count % a == 0 && count % b != 0){
-				System.out.println("Fizz");
-			}else if(count % b == 0 && count % a != 0){
-				System.out.println("Buzz");
-			}else if(count % c == 0){
-				System.out.println("FizzBuzz");
-			}else{
-				System.out.println(count);
-			}
-			count++;
-		}
+		a = scan.nextInt();
+		b = scan.nextInt();
+		int c = a * b;
+		if (a != 0 && b != 0) {
 
+			while (count < 101) {
+				if (count % a == 0 && count % b != 0) {
+					System.out.println("Fizz");
+				} else if (count % b == 0 && count % a != 0) {
+					System.out.println("Buzz");
+				} else if (count % c == 0) {
+					System.out.println("FizzBuzz");
+				} else {
+					System.out.println(count);
+				}
+				count++;
+			}
+		} else {
+			System.out.println("０ではできません");
+
+		}
 	}
 
 }
